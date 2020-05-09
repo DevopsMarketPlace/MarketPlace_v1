a186e27a1b30a96748b136085b0ca528aedfc41c
chaitankris <chaitanyakrishna119@gamail.com>
2020-05-10 01:44:24 +0530

    Added Basics Api's V:2

--- src/main/java/com/iiitb/spe/market_place_v1/Customer/CustomerController.java
+++ src/main/java/com/iiitb/spe/market_place_v1/Customer/CustomerController.java
@@ -1,4 +1,35 @@
 package com.iiitb.spe.market_place_v1.Customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerRepo repo;
+    @GetMapping("/Customer/{uid}")
+    public Optional<Customer> getCustomer(@PathVariable int uid)
+    {
+        return repo.findById(uid);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/Customer")
+    public String addcustomer(@RequestBody Customer customer){
+        repo.save(customer);
+        return "Registered Successfully";
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "/Customer/{uid}")
+    public String updatecustomer(@RequestBody Customer customer, @PathVariable int uid){
+        repo.save(customer);
+        return "Updated Successfully";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/Customer/{uid}")
+    public String deletecustomer(@RequestBody Customer customer, @PathVariable int uid){
+          repo.deleteById(uid);
+          return "Removed Successfully";
+    }
 }
--- src/main/java/com/iiitb/spe/market_place_v1/Customer/CustomerRepo.java
+++ src/main/java/com/iiitb/spe/market_place_v1/Customer/CustomerRepo.java
@@ -1,4 +1,6 @@
 package com.iiitb.spe.market_place_v1.Customer;
 
-public class CustomerRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer,Integer> {
 }
