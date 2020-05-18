112659cd9c9aec08ac9ee6aa4f6012e901a14fd7
Krishna Chaitanya Thottempudi <chaitanyakrishna119@gmail.com>
2020-05-18 22:57:58 +0530

    Resolved customer repo conflicts

--- src/main/java/com/iiitb/spe/market_place_v1/Customer/CustomerRepo.java
+++ src/main/java/com/iiitb/spe/market_place_v1/Customer/CustomerRepo.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-<<<<<<< HEAD
 import java.util.Optional;
 
 public interface CustomerRepo extends JpaRepository<Customer,Integer> {
@@ -12,10 +11,4 @@ public interface CustomerRepo extends JpaRepository<Customer,Integer> {
     @Query("select c from Customer  c join fetch c.orderList where c.uid=:id")
     Optional<Customer> fetchOrders(@Param("id")int id);
     Optional<Customer> findByUsername(String username);
-=======
-public interface CustomerRepo extends JpaRepository<Customer,Integer> {
-
-    @Query("select c from Customer  c join fetch c.orderList where c.uid=:id")
-    Customer fetchOrders(@Param("id")int id);
->>>>>>> cc728399fe099b69fdcf1333f42860a538faea55
 }
